2946156d3a54bff56b807b346ef3b30d930f69ec
PrakarshSingh5 <prakarshsinghlmp@gmail.com>
2023-03-28 16:36:20 +0530

    new project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState,useEffect} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export default function App(){
+  const[Book,setBook]=useState(0);
+  //when render method called then after that useeffect calls automatically
+  //After using useeffect we see the increment and alert both 
+useEffect(()=>{
+  alert('Book increased');
+})
+
+  return ( 
+  
+    <button onClick={(()=>{
+      setBook(Book+1);
+    })}
+    //when we use a alert function  as a inline function ,
+    // when click on  button then there is no increase only
+    // a alert message comes on clicking the button 
+    //basically we see the upper function is not working it directly comes to 
+    //the alert function 
+    //we want the increment and alert also hence we use the useEffect as it 
+    //run after the render function works then useEffect runs 
+    // ()=> {
+    //   alert('Book increased');
+    // } )
+  
+    >MathsBook-{Book}</button>
+   
   );
 }
 
-export default App;
